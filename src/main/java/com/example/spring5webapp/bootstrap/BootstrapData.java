d2e359d3e882fae91c59a73e024b796fb5745c15
Muhammed Gaygisiz <muhammed.gaygisiz@akros.ch>
2020-02-14 13:17:43 +0100

    Add publisher entity with repository

--- src/main/java/com/example/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/example/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package com.example.spring5webapp.bootstrap;
 
 import com.example.spring5webapp.domain.Author;
 import com.example.spring5webapp.domain.Book;
+import com.example.spring5webapp.domain.Publisher;
 import com.example.spring5webapp.repositories.AuthorRepository;
 import com.example.spring5webapp.repositories.BookRepository;
+import com.example.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,16 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(
+            AuthorRepository authorRepository,
+            BookRepository bookRepository,
+            PublisherRepository publisherRepository
+    ) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -40,5 +48,16 @@ public class BootstrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
+
+        Publisher dummyPublisher = new Publisher();
+        dummyPublisher.setName("Dummy Publisher Ltd.");
+        dummyPublisher.setAddressLine1("Some Avenue 7");
+        dummyPublisher.setZip("3012");
+        dummyPublisher.setCity("Bern");
+        dummyPublisher.setState("Swiss");
+
+        publisherRepository.save(dummyPublisher);
+
+        System.out.println("Number of publishers: " + publisherRepository.count());
     }
 }
