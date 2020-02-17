ccf4ab43eb1b520638d4370a67e63cf91510cb9a
Muhammed Gaygisiz <muhammed.gaygisiz@akros.ch>
2020-02-17 09:39:22 +0100

    Get bean from spring context

--- src/main/java/com/example/dependencyinjection/DependencyInjectionApplication.java
+++ src/main/java/com/example/dependencyinjection/DependencyInjectionApplication.java
@@ -1,13 +1,23 @@
 package com.example.dependencyinjection;
 
+import com.example.dependencyinjection.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+		ApplicationContext ctx
+				= SpringApplication.run(DependencyInjectionApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
