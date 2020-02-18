06a19148af95d57a8c74df85dd2e9c4472e1e082
Muhammed Gaygisiz <muhammed.gaygisiz@akros.ch>
2020-02-18 14:09:20 +0100

    Impl Map based Services, closes

---
+++ pet-clinic-data/src/main/java/com/example/spring5webapp/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.example.spring5webapp.services.map;
+
+import com.example.spring5webapp.model.Owner;
+import com.example.spring5webapp.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
