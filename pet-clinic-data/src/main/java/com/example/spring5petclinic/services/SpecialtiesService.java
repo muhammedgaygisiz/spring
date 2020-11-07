2be0d4379830e87abb399063a83d29dcdef3e35a
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-11-07 12:29:26 +0100

    Added SpecialtiesService Map Impl

---
+++ pet-clinic-data/src/main/java/com/example/spring5petclinic/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package com.example.spring5petclinic.services;
+
+import com.example.spring5petclinic.model.Specialty;
+
+public interface SpecialtiesService
+    extends CrudService<Specialty, Long> {
+
+}
