18af3946cdb7d2783fe23f7039941aebe742d8ff
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-11-08 16:06:17 +0100

    Added visit entity

--- pet-clinic-data/src/main/java/com/example/spring5petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/example/spring5petclinic/model/Specialty.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "specialty")
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
     @Column(name = "description")
