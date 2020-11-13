a2839b1934f8b34ccf0d6cc362b678a851f1618e
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-11-13 23:22:26 +0100

    Implemented Jpa Visit Service, added loader data

---
+++ pet-clinic-data/src/main/java/com/example/spring5petclinic/services/springdatajpa/VisitSdJpaService.java
@@ -0,0 +1,43 @@
+package com.example.spring5petclinic.services.springdatajpa;
+
+import com.example.spring5petclinic.model.Visit;
+import com.example.spring5petclinic.repositories.VisitRepository;
+import com.example.spring5petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSdJpaService implements VisitService {
+
+  private final VisitRepository visitRepository;
+
+  public VisitSdJpaService(VisitRepository visitRepository) {
+    this.visitRepository = visitRepository;
+  }
+
+  @Override public Set<Visit> findAll() {
+    Set<Visit> visits = new HashSet<>();
+    visitRepository.findAll().forEach(visits::add);
+    return visits;
+  }
+
+  @Override public Visit findById(Long id) {
+    return visitRepository.findById(id).orElse(null);
+  }
+
+  @Override public Visit save(Visit visit) {
+    return visitRepository.save(visit);
+  }
+
+  @Override public void deleteById(Long id) {
+    visitRepository.deleteById(id);
+  }
+
+  @Override public void delete(Visit visit) {
+    visitRepository.delete(visit);
+  }
+}
--- pet-clinic-web/src/main/java/com/example/spring5petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/spring5petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.example.spring5petclinic.bootstrap;
 
 import com.example.spring5petclinic.model.*;
-import com.example.spring5petclinic.services.OwnerService;
-import com.example.spring5petclinic.services.PetTypeService;
-import com.example.spring5petclinic.services.SpecialtyService;
-import com.example.spring5petclinic.services.VetService;
+import com.example.spring5petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,16 +14,19 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     public DataLoader(
-            OwnerService ownerService,
-            VetService vetService,
-            PetTypeService petTypeService,
-            SpecialtyService specialtyService) {
+        OwnerService ownerService,
+        VetService vetService,
+        PetTypeService petTypeService,
+        SpecialtyService specialtyService, VisitService visitService
+    ) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -92,6 +92,12 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionatsCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
