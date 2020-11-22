146303ba558a09d8aaf9e915917d79a2f2505232
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-11-22 10:27:08 +0100

    Add RequestMapping for pet detail view

--- pet-clinic-web/src/main/java/com/example/spring5petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/spring5petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.example.spring5petclinic.controllers;
 import com.example.spring5petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,5 +28,14 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav =
+            new ModelAndView("owners/ownerDetails");
+
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
